972c82b77a8b5db0c0d0c5b7f33fac579ecca54e
Subin Cho <subinee1633@gmail.com>
2021-10-29 14:46:42 -0400

    added models and controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController 
+    def create
+        @user = User.find_by_credentials(user_params)
+        if @user
+            login!(@user)
+            redirect_to root_url
+        else
+            flash.now[:errors] = @user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        logout!
+        redirect_to root_url
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if user.save!
+            login!(@user)
+            redirect_to root_url
+        else
+            flash.now[:errors] = @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+end
