e3525c6387e3109184d993b6a6da20b949eeffa8
Subin Cho <subinee1633@gmail.com>
2021-11-01 22:31:52 -0400

    added to seed for items

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,7 +1,10 @@
 class Api::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    render 'api/items/index'
   end
 
   def show
+    @item = Item.find_by(id: params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
     validates :shop_id, :category_id, presence: true
     validates :title, :description, :image_url, :price, :fav_count, presence: true
+    validates :avg_rating, inclusion: { in: (1..5) }
     validates :favorite, inclusion: { in: [true, false]}
 
     belongs_to :shop
