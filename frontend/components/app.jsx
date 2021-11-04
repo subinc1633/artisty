b5dc3c8becf9a80db05c26d7fe91e9ec07d136d0
Subin Cho <subinee1633@gmail.com>
2021-11-04 18:10:35 -0400

    fixed user profile bug

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,16 +5,16 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import UserShowContainer from './users/user_show_container';
 import Modal from './modal/modal';
 import ItemShowContainer from './items/item_show_container';
-// import CategoryIndexContainer from './categories/category_splash_container';
+import NavBarContainer from './nav_bar/nav_bar_container';
 
 const App = () => {
     return (
         <div style={{backgroundColor: 'white'}}>
+            <NavBarContainer />
             <Modal />
             <Switch>
                 <Route exact path='/' component={Splash} />
                 <Route path='/users/:userId' component={UserShowContainer} />
-                {/* <Route path='/categories' component={CategoryIndexContainer} /> */}
                 <Route path='/items/:itemId' component={ItemShowContainer} />
                 <Redirect to='/404' />
             </Switch>
