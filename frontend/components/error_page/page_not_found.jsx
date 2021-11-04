8fea18533ab1f6924e143f72e88617e731588eb9
Subin Cho <subinee1633@gmail.com>
2021-11-04 11:27:53 -0400

    edited 404 page

--- frontend/components/error_page/page_not_found.jsx
+++ frontend/components/error_page/page_not_found.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PageNotFound = () => (
-    <section className='page-not-found'>
-        <div className='page-items'>
+    <div className='not-found-background'>
+        <nav className='not-found-bar'>
+            <Link to='/'><h2 className='artisty'>Artisty</h2></Link>
+        </nav>
+        <section className='page-not-found'>
             <h1>Ooops... nothing to see here!</h1>
                 <p>Sorry, the page you're looking for is not found.</p>
                 <input className='searchbar' type='text' placeholder='Search for anything' />
                     <button type='submit'>Submit</button><br/><br/>
                 <Link to='/'>Go back to Artisty.com</Link>
-        </div>
-    </section>
+        </section>
+    </div>
 );
 
 export default PageNotFound;
