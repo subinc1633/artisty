5b241b337e5125af26dba75db74ca43e65254a1f
Subin Cho <78716708+subinc1633@users.noreply.github.com>
2021-12-20 01:03:47 -0500

    edit nav bar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 import { IoLogoGithub, IoLogoLinkedin, IoCartOutline, IoPersonCircle, IoCaretDown } from 'react-icons/io5';
+import { FaAngellist, FaUserTie } from 'react-icons/fa';
 import NavBarLink from './nav_bar_links';
 import SearchBar from './search_bar';
 import Dropdown from './dropdown';
@@ -47,10 +48,14 @@ const NavigationBar = (props) => {
             <ul className="top-nav">
                 <li><Link to="/"><h2 className="artisty">Artisty</h2></Link></li>
                 <li className="nav-searchbar"><SearchBar /></li>
-                <a href="https://github.com/subinc1633" target="_blank"><button className="nav-contact-button"><IoLogoGithub /></button></a>
-                <a href="https://www.linkedin.com/in/sc1633/" target="_blank"><button className="nav-contact-button"><IoLogoLinkedin /></button></a>
-                <li>{currentUser ? <button className="user-button" onClick={() => toggleDropdown()}><IoPersonCircle /> <IoCaretDown className="caret-button" /></button> : <button className="sign-in-button" onClick={() => signIn()}>Sign in</button>}</li>
-                <li><button className="cart-button" onClick={() => checkIfLoggedIn()}><IoCartOutline /></button></li>
+                <li className="nav-buttons">
+                    <a href="https://github.com/subinc1633" target="_blank"><button className="nav-contact-button"><IoLogoGithub /></button></a>
+                    <a href="https://www.linkedin.com/in/sc1633/" target="_blank"><button className="nav-contact-button"><IoLogoLinkedin /></button></a>
+                    <a href="https://angel.co/u/subin-cho" target="_blank"><button className="nav-contact-button"><FaAngellist /></button></a>
+                    <a href="http://subincho.net/" target="_blank"><button className="nav-contact-button"><FaUserTie /></button></a>    
+                    {currentUser ? <button className="user-button" onClick={() => toggleDropdown()}><IoPersonCircle /> <IoCaretDown className="caret-button" /></button> : <button className="sign-in-button" onClick={() => signIn()}>Sign in</button>}
+                    <button className="cart-button" onClick={() => checkIfLoggedIn()}><IoCartOutline /></button>
+                </li>
             </ul><br/>
             <Dropdown currentUser={currentUser} logout={logout} display={display} onClick={e => e.stopPropagation()} />
             <ul className="bottom-nav">
